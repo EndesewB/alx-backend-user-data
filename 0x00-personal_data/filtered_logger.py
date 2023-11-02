670e9e18baefd7c384564527440e265e3437f9e6
endesew bantayehu yenesew <endesew12@gmail.com>
2023-11-02 07:32:42 +0000

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,6 @@ def filter_datum(fields, redaction, message, separator):
     """
     this method filters the personal data
     """
-    regex = fr'({"|".join(re.escape(field) for field\
-               in fields)})=[^{separator}]+'
+    regex = fr'({"|".join(re.escape(field) for field in fields)})=\
+    [^{separator}]+'
     return re.sub(regex, fr'\1={redaction}', message)
