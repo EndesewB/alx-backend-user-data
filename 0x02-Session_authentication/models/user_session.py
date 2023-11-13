5212071fbb751b63228158fc4a46d1d5b9a2e2ad
Endesew Bantayehu <117736159+EndesewB@users.noreply.github.com>
2023-11-13 15:29:41 +0300

    Update user_session.py

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -6,36 +6,6 @@ from sqlalchemy import Column, String, ForeignKey
 from models.base import Base
 
 
-class ForeignKey:
-    """Class representing a foreign key in a database table.
-    """
-    
-    def __init__(self, table, column, _table_key):
-        """Initialize a ForeignKey instance.
-
-        Args:
-            table (str): The name of the referenced table.
-            column (str): The name of the referenced column.
-            _table_key (str): The key used to relate the tables.
-        """
-        self.table = table
-        self.column = column
-        self._table_key = _table_key
-
-    def __repr__(self):
-        """Return a string representation of the ForeignKey instance.
-        """
-        return f"ForeignKey(table={self.table}, column={self.column}, _table_key={self._table_key})"
-
-    def _table_key(self):
-        """Get the key used to relate the tables.
-
-        Returns:
-            str: The key used to relate the tables.
-        """
-        return self._table_key
-
-
 class UserSession(Base):
     """UserSession class inherits from Base
     """
